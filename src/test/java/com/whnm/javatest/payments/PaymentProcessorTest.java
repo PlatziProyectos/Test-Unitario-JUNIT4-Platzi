b1c5b7386cde13c8a8114f49cc8d98ec8de48794
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2021-05-29 05:01:50 -0500

    estructuracion ideal de las partes de un test y metodo before

--- src/test/java/com/whnm/javatest/payments/PaymentProcessorTest.java
+++ src/test/java/com/whnm/javatest/payments/PaymentProcessorTest.java
@@ -1,34 +1,47 @@
 package com.whnm.javatest.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setUp() throws Exception {
+        paymentGateway =
+                Mockito.mock(PaymentGateway.class);
+        paymentProcessor =
+                new PaymentProcessor(paymentGateway);
+    }
 
     @Test
     public void paymentIsCorrect() {
-        PaymentGateway paymentGateway =
-                Mockito.mock(PaymentGateway.class);
+        /*
+         * Parte 1: Preparacion de los datos
+         */
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentProcessor paymentProcessor =
-                new PaymentProcessor(paymentGateway);
 
-        assertTrue(paymentProcessor.makePayment(1000));
+        /*
+         * Parte 2: Ejecucion del metodo objetivo
+         */
+        boolean result = paymentProcessor.makePayment(1000);
+
+        /*
+         * Parte 3: Comprobacion del resultado
+         */
+        assertTrue(result);
     }
 
     @Test
     public void paymentIsNoCorrect() {
-        PaymentGateway paymentGateway =
-                Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-
-        PaymentProcessor paymentProcessor =
-                new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
     }
