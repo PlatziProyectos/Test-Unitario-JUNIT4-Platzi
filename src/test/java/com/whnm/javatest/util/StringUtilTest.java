fad60edd565d47f6a975fcf5e04bcad09aa4800c
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2021-05-29 03:39:35 -0500

    test unitarios manuales con junit correctos

--- src/test/java/com/whnm/javatest/util/StringUtilTest.java
+++ src/test/java/com/whnm/javatest/util/StringUtilTest.java
@@ -1,19 +1,10 @@
 package com.whnm.javatest.util;
 
+import org.junit.Assert;
+
 public class StringUtilTest {
     public static void main(String[] args) {
-        String word = StringUtil.repeat("hola", 3);
-        assertEquals(word, "holaholahol");
-
-        String word2 = StringUtil.repeat("hola", 1);
-        assertEquals(word2, "hola");
-    }
-
-    private static void assertEquals(String resultado, String resultadoEsperado) {
-
-        if (!resultado.equals(resultadoEsperado)){
-            throw new RuntimeException("ERROR");
-        }
-
+        Assert.assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
+        Assert.assertEquals(StringUtil.repeat("hola", 1), "hola");
     }
 }
