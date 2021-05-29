fe93e263381a6807fdba023e640eb1958d3eeefe
Wilson Hernande Neira Mija <wilsonnm22@hotmail.com>
2021-05-29 05:59:53 -0500

    refactorizando anno bisiesto

--- src/main/java/com/whnm/javatest/util/DateUtil.java
+++ src/main/java/com/whnm/javatest/util/DateUtil.java
@@ -6,8 +6,7 @@ public class DateUtil {
 
         if ((year % Constantes.INT_4 == 0 &&
                 year % Constantes.INT_100 != 0)
-          || (year % Constantes.INT_100 == 0 &&
-                year % Constantes.INT_400 == 0)) {
+          || year % Constantes.INT_400 == 0) {
             return true;
         } else {
             return false;
